c2db26d609273dafc7f21f291426c3387bb2a695
Dozingfiretruck <1473454180@qq.com>
2022-11-22 10:52:11 +0800

    add:添加合宙版权声明

--- interface/src/bsp_spi.c
+++ interface/src/bsp_spi.c
@@ -1,40 +1,24 @@
 /*
- * Copyright (c) 2013-2016 ARM Limited. All rights reserved.
+ * Copyright (c) 2022 OpenLuat & AirM2M
  *
- * SPDX-License-Identifier: Apache-2.0
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
  *
- * Licensed under the Apache License, Version 2.0 (the License); you may
- * not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
- * www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an AS IS BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-  *
- * $Date:        12. July 2018
- * $Revision:    V2.0
- *
- * Driver:       Driver_SPI0, Driver_SPI1, Driver_SPI2
- * Configured:   via RTE_Device.h configuration file
- * Project:      SPI (SSP used for SPI) Driver for EC618
- * --------------------------------------------------------------------------
- * Use the following configuration settings in the middleware component
- * to connect to this driver.
- *
- *   Configuration Setting                 Value   SPI Interface
- *   ---------------------                 -----   -------------
- *   Connect to hardware via Driver_SPI# = 0       use SPI0 (SSP0)
- *   Connect to hardware via Driver_SPI# = 1       use SPI1 (SSP1)
- * -------------------------------------------------------------------------- */
-
-/* History:
- *  Version 2.0
- *    - Initial CMSIS Driver API V2.0 release
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 
 #include "bsp_spi.h"
 #include "slpman.h"
