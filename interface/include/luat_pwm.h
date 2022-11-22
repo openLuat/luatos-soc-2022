2298074c33017ceaccef4bb0e588591b6d6b6ba5
liangjian <liangjian@airm2m.com>
2022-11-22 21:40:17 +0800

    add:增加pwm 输出，尚未解决1.硬件定时器判断，2.输出个数控制，3.获取当前频率

---
+++ interface/include/luat_pwm.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2022 OpenLuat & AirM2M
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+#ifndef LUAT_PWM_H
+#define LUAT_PWM_H
+/**
+ *@version V1.0
+ *@attention
+ */
+/**
+ * @ingroup luatos_device 外设接口
+ * @{
+ */
+#include "luat_base.h"
+
+typedef struct luat_pwm_conf {
+    int channel;       /*PWM通道*/
+    size_t period;   /*频率, 1-26000hz*/
+    size_t pulse;    /*占空比*/
+    size_t pnum;     /*输出周期 0为持续输出, 1为单次输出, 其他为指定脉冲数输出*/
+    size_t precision;  /*分频精度, 100/256/1000, 默认为100, 若设备不支持会有日志提示*/
+} luat_pwm_conf_t;
+
+
+/**
+ * @brief 配置pwm 参数
+ * 
+ * @param id i2c_id
+ * @return int 
+ */
+
+int luat_pwm_setup(luat_pwm_conf_t* conf);
+
+/**
+ * @brief 打开pwm 通道
+ * 
+ * @param id i2c_id
+ * @return int 
+ */
+
+int luat_pwm_open(int channel, size_t period, size_t pulse, int pnum);
+
+/**
+ * @brief 获取pwm 频率
+ * 
+ * @param id i2c_id
+ * @return int 
+ */
+
+int luat_pwm_capture(int channel,int freq);
+
+/**
+ * @brief 关闭pwm 接口
+ * 
+ * @param id i2c_id
+ * @return int 
+ */
+int luat_pwm_close(int channel);
+
+#endif
---
+++ interface/src/luat_pwm_ec618.c
@@ -0,0 +1,196 @@
+/*
+ * Copyright (c) 2022 OpenLuat & AirM2M
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * PWM操作
+ * 
+ */
+#include "common_api.h"
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "luat_base.h"
+#ifdef __LUATOS__
+#include "luat_malloc.h"
+#include "luat_msgbus.h"
+#include "luat_timer.h"
+#endif
+#include "luat_pwm.h"
+
+#include "timer.h"
+#include "hal_adc.h"
+#include "ic.h"
+#include "hal_trim.h"
+#include "timer.h"
+#include "ec618.h"
+#include "clock.h"
+#include "pad.h"
+#include "luat_debug.h"
+#include "RTE_Device.h"
+#define EIGEN_TIMER(n)             ((TIMER_TypeDef *) (AP_TIMER0_BASE_ADDR + 0x1000*n))
+
+
+
+
+static char if_initialized_timer(const int channel)
+{
+    
+  if( 0 != EIGEN_TIMER(channel)->TCTLR )
+    {
+        return -1;
+    }
+    return 0;
+}
+
+static void Timer_ISR()
+{
+    // for( char i =0 ;i< 6;i++)
+    // {
+    //     if (TIMER_getInterruptFlags((uint32_t)pwm_source[i]) & TIMER_MATCH0_INTERRUPT_FLAG)
+    //     {
+    //         TIMER_clearInterruptFlags((uint32_t)i, TIMER_MATCH0_INTERRUPT_FLAG);
+    //         LUAT_DEBUG_PRINT("Timer_ISR in %d",i);
+    //     }
+    //     else
+    //     {
+    //         LUAT_DEBUG_PRINT("Timer_ISR not in %d",i);
+    //     }
+    // }
+}
+
+int luat_pwm_open(const int channel,const size_t period, const size_t pulse,const int pnum) {
+
+    PadConfig_t config;
+    TimerPwmConfig_t pwmConfig;
+    unsigned int clockId,clockId_slect,time_req;
+    // LUAT_DEBUG_PRINT("luat_pwm_open channel:%d perio:%d pulse:%d pnum:%d",channel,period,pulse,pnum);
+    int ret =0;
+    if ( channel > 5 || channel < 0)
+        return -1;
+    if (period > 26000 || period < 0)
+        return -2;
+    if (pulse > 100 || pulse < 0 )
+        return -3;
+    if(if_initialized_timer(channel) < 0)
+    {
+        return -4;   // hardware timer is used
+    }
+    switch(channel)
+    {
+        case  0:
+            clockId =  FCLK_TIMER0;
+            clockId_slect = FCLK_TIMER0_SEL_26M;
+            time_req = PXIC0_TIMER0_IRQn;
+        case  1:
+            clockId =  FCLK_TIMER1;
+            clockId_slect = FCLK_TIMER1_SEL_26M;
+            time_req = PXIC0_TIMER1_IRQn;
+        case  2:
+            clockId =  FCLK_TIMER2;
+            clockId_slect = FCLK_TIMER2_SEL_26M;
+            time_req = PXIC0_TIMER2_IRQn;
+        case  3:
+            clockId =  FCLK_TIMER3;
+            clockId_slect = FCLK_TIMER3_SEL_26M;
+            time_req = PXIC0_TIMER3_IRQn;
+        case  4:
+            clockId =  FCLK_TIMER4;
+            clockId_slect = FCLK_TIMER4_SEL_26M;
+            time_req = PXIC0_TIMER4_IRQn;
+        case  5:
+            clockId =  FCLK_TIMER5;
+            clockId_slect = FCLK_TIMER5_SEL_26M;
+            time_req = PXIC0_TIMER5_IRQn;
+        default :
+            break;
+    }
+    // LUAT_DEBUG_PRINT("luat_pwm_open get timer_id %d",timer_id);
+    PAD_getDefaultConfig(&config);
+    config.mux = PAD_MUX_ALT5;
+    
+    
+#if RTE_PWM0
+    if(0 == channel){
+        PAD_setPinConfig(RTE_PWM0, &config);   
+    }
+#endif
+#if RTE_PWM1
+    if(1 == channel){
+        PAD_setPinConfig(RTE_PWM1, &config);
+    }
+#endif
+#if RTE_PWM2
+    if(2 == channel){
+        PAD_setPinConfig(RTE_PWM2, &config);
+    }
+#endif
+#if RTE_PWM3
+    if(3 == channel){
+        PAD_setPinConfig(RTE_PWM3, &config);
+    }
+#endif
+#if RTE_PWM4
+    if(4 == channel){
+        PAD_setPinConfig(RTE_PWM4, &config);
+    }
+#endif
+#if RTE_PWM5
+    if( 5 == channel){
+        PAD_setPinConfig(RTE_PWM5, &config);
+    }
+#endif
+    CLOCK_setClockSrc(clockId, clockId_slect);
+    CLOCK_setClockDiv(clockId, 1);
+    TIMER_driverInit();
+
+    pwmConfig.pwmFreq_HZ = period;
+    pwmConfig.srcClock_HZ = GPR_getClockFreq(clockId);  
+    pwmConfig.dutyCyclePercent = pulse;
+    TIMER_setupPwm(channel, &pwmConfig);
+    
+    // TIMER_interruptConfig(timer_id, TIMER_MATCH0_SELECT, TIMER_INTERRUPT_LEVEL);
+    // TIMER_interruptConfig(timer_id, TIMER_MATCH1_SELECT, TIMER_INTERRUPT_DISABLED);
+    // TIMER_interruptConfig(timer_id, TIMER_MATCH2_SELECT, TIMER_INTERRUPT_DISABLED);
+
+    // XIC_SetVector(time_req, Timer_ISR);
+    // XIC_EnableIRQ(time_req);
+    TIMER_start(channel);
+
+    return 0;
+}
+
+int luat_pwm_setup(luat_pwm_conf_t* conf)
+{
+    return luat_pwm_open(conf->channel,conf->period,conf->pulse,conf->pnum);
+}
+
+int luat_pwm_capture(int channel,int freq)
+{
+	
+    return EIGEN_TIMER(channel)->TMR[0];
+}
+
+int luat_pwm_close(int channel)
+{
+    TIMER_stop(channel);
+    return 0;
+}
+
