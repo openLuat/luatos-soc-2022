b9eb4823d3b3c8c322c6c517d5684680f758de82
wanglong <wanglongqyt@163.com>
2022-11-27 17:16:41 +0800

    update:设置程序为不同的模块

--- project/web_audio/inc/web_audio.h
+++ project/web_audio/inc/web_audio.h
@@ -1,6 +1,5 @@
-#ifndef _YULABAYANSHI_H
-#define _UNLABAYANSHI_H
-
+#ifndef _WEB_AUDIO_H
+#define _WEB_AUDIO_H
 #include "string.h"
 
 #include "common_api.h"
@@ -25,51 +24,7 @@
 #define User "username"  
 #define Password "password" 
 
-//AIR780E+TM8211ø™∑¢∞Â≈‰÷√
-#define CODEC_PWR_PIN HAL_GPIO_12
-#define CODEC_PWR_PIN_ALT_FUN	4
-#define PA_PWR_PIN HAL_GPIO_25
-#define PA_PWR_PIN_ALT_FUN	0
-#define LED2_PIN	HAL_GPIO_24
-#define LED2_PIN_ALT_FUN	0
-#define LED3_PIN	HAL_GPIO_23
-#define LED3_PIN_ALT_FUN	0
-#define LED4_PIN	HAL_GPIO_27
-#define LED4_PIN_ALT_FUN	0
-#define CHARGE_EN_PIN	HAL_GPIO_2
-#define CHARGE_EN_PIN_ALT_FUN	0
 
-#define NET_LED_PIN HAL_GPIO_27
-typedef struct
-{
-    uint32_t priority;
-    uint32_t playType;
-    union
-    {
-        struct
-        {
-            char *data;
-            uint8_t len;
-        } tts;
-        struct
-        {
-            audio_play_info_t *info;
-            uint8_t count;
-        } file;
-    } message;
-    void * userParam;
-} audioQueueData;
 
-typedef enum
-{
-    MONEY_PLAY = 0,
-    PAD_PLAY,
-    SYS_PLAY
-} AUDIO_PLAY_PRIORITY;
 
-typedef enum
-{
-    TTS_PLAY = 0,
-    FILE_PLAY,
-} AUDI;
 #endif
