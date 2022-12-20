9cdf367a55206f52d3683db740966f36f56194cc
doudou <1491766334@qq.com>
2022-12-20 21:21:39 +0800

    update: 简单区分一下mqtt发送结果

--- project/cloud_speaker/inc/mqtt_publish.h
+++ project/cloud_speaker/inc/mqtt_publish.h
@@ -6,4 +6,13 @@ typedef struct mqtt_publish_para
     mqttSendMsg* publish;
     void (*callback)(int result);
 }mqtt_publish_para_t;
+
+
+typedef enum MQTT_PUBLISH_RESULT
+{
+    MQTT_PUBLISH_SUCCESS,
+    MQTT_PUBLISH_FAIL,
+    MQTT_NOT_READY,
+    NETWORK_NOT_READY
+}MQTT_PUBLISH_RESULT_E;
 #endif
