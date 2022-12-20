92221b5a1566ce9ad9bbefe96a0be7dfbf9c2469
Wendal Chen <wendal1985@gmail.com>
2022-12-20 16:31:04 +0800

    update: 调整pwm适配,支持1x系列的固定映射, 同时兼容0-5的映射, 从而释放780e开发板的更多pwm通道
    https://gitee.com/openLuat/LuatOS/issues/I66SYB

--- interface/src/luat_pwm_ec618.c
+++ interface/src/luat_pwm_ec618.c
@@ -34,6 +34,7 @@
 #include "luat_timer.h"
 #endif
 #include "luat_pwm.h"
+#include "luat_rtos.h"
 
 #include "timer.h"
 #include "hal_adc.h"
@@ -62,7 +63,7 @@ static signed char if_initialized_timer(const int channel)
 
 
 static int g_s_pnum_set[PWM_CH_MAX] = {0}; /*设置PWM脉冲个数*/
-volatile static int g_s_pnum_update[PWM_CH_MAX] = {0};/*当前PWM个数*/
+static volatile int g_s_pnum_update[PWM_CH_MAX] = {0};/*当前PWM个数*/
 /*最高频率应是26M*/ 
 #define MAX_FREQ (26*1000*1000)
 typedef struct pwm_ctx
@@ -73,13 +74,73 @@ typedef struct pwm_ctx
     uint32_t pnum;
 }pwm_ctx_t;
 
+typedef struct pwm_map
+{
+    int pwm_ch; // 对外展示的pwm id
+    int clockId;
+    int clockId_slect;
+    int time_req;
+    int pad;
+}pwm_map_t;
+
+static const pwm_map_t maps[] = {
+#ifdef RTE_PWM0
+    {.pwm_ch=0, .clockId=FCLK_TIMER0, .clockId_slect=FCLK_TIMER0_SEL_26M, .time_req=PXIC0_TIMER0_IRQn, .pad=RTE_PWM0},
+#endif
+#ifdef RTE_PWM0
+    {.pwm_ch=1, .clockId=FCLK_TIMER1, .clockId_slect=FCLK_TIMER1_SEL_26M, .time_req=PXIC0_TIMER1_IRQn, .pad=RTE_PWM1},
+#endif
+#ifdef RTE_PWM0
+    {.pwm_ch=2, .clockId=FCLK_TIMER2, .clockId_slect=FCLK_TIMER2_SEL_26M, .time_req=PXIC0_TIMER2_IRQn, .pad=RTE_PWM2},
+#endif
+#ifdef RTE_PWM0
+    {.pwm_ch=3, .clockId=FCLK_TIMER3, .clockId_slect=FCLK_TIMER3_SEL_26M, .time_req=PXIC0_TIMER3_IRQn, .pad=RTE_PWM3},
+#endif
+#ifdef RTE_PWM0
+    {.pwm_ch=4, .clockId=FCLK_TIMER4, .clockId_slect=FCLK_TIMER4_SEL_26M, .time_req=PXIC0_TIMER4_IRQn, .pad=RTE_PWM4},
+#endif
+#ifdef RTE_PWM0
+    {.pwm_ch=5, .clockId=FCLK_TIMER5, .clockId_slect=FCLK_TIMER5_SEL_26M, .time_req=PXIC0_TIMER5_IRQn, .pad=RTE_PWM5},
+#endif
+
+    // 以下为固定映射
+    /*
+    10 -- GPIO 01 -- PAD 16  ---> LCD_RST
+    11 -- GPIO 02 -- PAD 17
+    12 -- GPIO 16 -- PAD 31
+    13 -- GPIO 17 -- PAD 32
+    14 -- GPIO 18 -- PAD 33  ---> UART1_RX
+    15 -- GPIO 19 -- PAD 34  ---> UART1_TX
+    */
+    {.pwm_ch=10, .clockId=FCLK_TIMER0, .clockId_slect=FCLK_TIMER0_SEL_26M, .time_req=PXIC0_TIMER0_IRQn, .pad=16},
+    {.pwm_ch=11, .clockId=FCLK_TIMER1, .clockId_slect=FCLK_TIMER1_SEL_26M, .time_req=PXIC0_TIMER1_IRQn, .pad=17},
+    {.pwm_ch=12, .clockId=FCLK_TIMER2, .clockId_slect=FCLK_TIMER2_SEL_26M, .time_req=PXIC0_TIMER2_IRQn, .pad=31},
+    {.pwm_ch=13, .clockId=FCLK_TIMER3, .clockId_slect=FCLK_TIMER3_SEL_26M, .time_req=PXIC0_TIMER3_IRQn, .pad=32},
+    {.pwm_ch=14, .clockId=FCLK_TIMER4, .clockId_slect=FCLK_TIMER4_SEL_26M, .time_req=PXIC0_TIMER4_IRQn, .pad=33},
+    {.pwm_ch=15, .clockId=FCLK_TIMER5, .clockId_slect=FCLK_TIMER5_SEL_26M, .time_req=PXIC0_TIMER5_IRQn, .pad=34},
+
+};
+
+
 
 static pwm_ctx_t pwms[PWM_CH_MAX];
 
-PLAT_PA_RAMCODE volatile static void Timer_ISR()
+static int luat_pwm_mapid(int channel) {
+    int map_id = -1;
+    for (size_t i = 0; i < sizeof(maps) / sizeof(pwm_map_t); i++)
+    {
+        if (maps[i].pwm_ch == channel) {
+            map_id = i;
+            break;
+        }
+    }
+    return map_id;
+}
+
+PLAT_PA_RAMCODE static void Timer_ISR()
 {
 
-    volatile static int i = 0;
+    volatile int i = 0;
 
     for(i = 0;i < PWM_CH_MAX;i++)
     {
@@ -101,111 +162,38 @@ PLAT_PA_RAMCODE volatile static void Timer_ISR()
 }
 
 int luat_pwm_open(int channel, size_t freq,  size_t pulse, int pnum) {
-
-    unsigned int clockId,clockId_slect;
-    IRQn_Type time_req;
-
     PadConfig_t config = {0};
 
-    // LUAT_DEBUG_PRINT("luat_pwm_open channel:%d perio:%d pulse:%d pnum:%d",channel,period,pulse,pnum);
-    if ( channel >= PWM_CH_MAX || channel < 0)
+    int map_id = luat_pwm_mapid(channel);
+    if (map_id == -1)
         return -1;
-    if (freq > MAX_FREQ || freq < 0)
+
+    if (freq > MAX_FREQ)
         return -2;
     if (pulse > 100)
         pulse = 100;
-    else if (pulse < 0)
-        pulse = 0;
 
-
-    if(if_initialized_timer(channel) < 0)
+    if(if_initialized_timer(channel % 10) < 0)
     {
         return -4;   // hardware timer is used
     }
 
-    switch(channel)
-    {
-        case  0:
-            clockId =  FCLK_TIMER0;
-            clockId_slect = FCLK_TIMER0_SEL_26M;
-            time_req = PXIC0_TIMER0_IRQn;
-            g_s_pnum_set[0] = pnum;
-            break;
-        case  1:
-            clockId =  FCLK_TIMER1;
-            clockId_slect = FCLK_TIMER1_SEL_26M;
-            time_req = PXIC0_TIMER1_IRQn;
-            g_s_pnum_set[1] = pnum;
-            break;
-        case  2:
-            clockId =  FCLK_TIMER2;
-            clockId_slect = FCLK_TIMER2_SEL_26M;
-            time_req = PXIC0_TIMER2_IRQn;
-            g_s_pnum_set[2] = pnum;
-            break;
-        case  3:
-            clockId =  FCLK_TIMER3;
-            clockId_slect = FCLK_TIMER3_SEL_26M;
-            time_req = PXIC0_TIMER3_IRQn;
-            g_s_pnum_set[3] = pnum;
-            break;
-        case  4:
-            clockId =  FCLK_TIMER4;
-            clockId_slect = FCLK_TIMER4_SEL_26M;
-            time_req = PXIC0_TIMER4_IRQn;
-            g_s_pnum_set[4] = pnum;
-            break;
-        case  5:
-            clockId =  FCLK_TIMER5;
-            clockId_slect = FCLK_TIMER5_SEL_26M;
-            time_req = PXIC0_TIMER5_IRQn;
-            g_s_pnum_set[5] = pnum;
-            break;
-        default :
-            // 其他复用通道暂不支持
-            return -5;
-    }
     // LUAT_DEBUG_PRINT("luat_pwm_open get timer_id %d",timer_id);
     PAD_getDefaultConfig(&config);
     config.mux = PAD_MUX_ALT5;
     
-    
-#if RTE_PWM0
-    if(0 == channel){
-        PAD_setPinConfig(RTE_PWM0, &config);   
-    }
-#endif
-#if RTE_PWM1
-    if(1 == channel){
-        PAD_setPinConfig(RTE_PWM1, &config);
-    }
-#endif
-#if RTE_PWM2
-    if(2 == channel){
-        PAD_setPinConfig(RTE_PWM2, &config);
-    }
-#endif
-#if RTE_PWM3
-    if(3 == channel){
-        PAD_setPinConfig(RTE_PWM3, &config);
-    }
-#endif
-#if RTE_PWM4
-    if(4 == channel){
-        PAD_setPinConfig(RTE_PWM4, &config);
-    }
-#endif
-#if RTE_PWM5
-    if( 5 == channel){
-        PAD_setPinConfig(RTE_PWM5, &config);
-    }
-#endif
-    CLOCK_setClockSrc(clockId, clockId_slect);
-    CLOCK_setClockDiv(clockId, 1);
+    // 为支持复用, 只取低位
+    channel = channel % 10;
+
+    g_s_pnum_set[channel] = pnum;
+    PAD_setPinConfig(maps[map_id].pad, &config);
+
+    CLOCK_setClockSrc(maps[map_id].clockId, maps[map_id].clockId_slect);
+    CLOCK_setClockDiv(maps[map_id].clockId, 1);
     TIMER_driverInit();
 
     pwms[channel].timer_config.pwmFreq_HZ = freq;
-    pwms[channel].timer_config.srcClock_HZ = GPR_getClockFreq(clockId);  
+    pwms[channel].timer_config.srcClock_HZ = GPR_getClockFreq(maps[map_id].clockId);  
     pwms[channel].timer_config.dutyCyclePercent = pulse;
     TIMER_setupPwm(channel, &pwms[channel].timer_config);
     if(0 != pnum)
@@ -214,34 +202,38 @@ int luat_pwm_open(int channel, size_t freq,  size_t pulse, int pnum) {
         TIMER_interruptConfig(channel, TIMER_MATCH1_SELECT, TIMER_INTERRUPT_DISABLED);
         TIMER_interruptConfig(channel, TIMER_MATCH2_SELECT, TIMER_INTERRUPT_LEVEL);
 
-        XIC_SetVector(time_req,Timer_ISR);
-        XIC_EnableIRQ(time_req);
+        XIC_SetVector(maps[map_id].time_req,Timer_ISR);
+        XIC_EnableIRQ(maps[map_id].time_req);
     }
 
     TIMER_start(channel);
 
     return 0;
 }
+
 int luat_pwm_update_dutycycle(int channel,size_t pulse)
 {
-    if (channel < 0 || channel >= PWM_CH_MAX) {
+    int map_id = luat_pwm_mapid(channel);
+    if (map_id == -1)
         return -1;
-    }
+    channel = channel % 10;
 
     pwms[channel].timer_config.dutyCyclePercent = pulse;
     TIMER_setupPwm(channel, &pwms[channel].timer_config);
-
     return 0;
 }
+
 int luat_pwm_setup(luat_pwm_conf_t* conf)
 {
     int channel = conf->channel;
-    if (channel < 0 || channel >= PWM_CH_MAX)
+    int map_id = luat_pwm_mapid(channel);
+    if (map_id == -1)
         return -1;
     if (conf->precision != 100) {
         // 当且仅支持100分辨率,等驱动重构完再改
         return -1;
     }
+    channel = channel % 10;
     // 判断一下是否只修改了占空比. 当且仅当频率相同,pnum为0(即持续输出),才支持单独变更
     if (pwms[channel].timer_config.pwmFreq_HZ == conf->period && conf->pnum == 0) {
         if (conf->pulse != pwms[channel].timer_config.dutyCyclePercent) {
@@ -254,18 +246,18 @@ int luat_pwm_setup(luat_pwm_conf_t* conf)
 
 int luat_pwm_capture(int channel,int freq)
 {
-    if (channel < 0 || channel >= PWM_CH_MAX) {
+    int map_id = luat_pwm_mapid(channel);
+    if (map_id == -1)
         return -1;
-    }
-    return EIGEN_TIMER(channel)->TMR[0];
+    return EIGEN_TIMER(channel % 10)->TMR[0];
 }
 
 int luat_pwm_close(int channel)
 {
-    if (channel < 0 || channel >= PWM_CH_MAX) {
+    int map_id = luat_pwm_mapid(channel);
+    if (map_id == -1)
         return -1;
-    }
-    luat_pwm_update_dutycycle(channel,0);
+    luat_pwm_update_dutycycle(channel, 0);
     luat_rtos_task_sleep(1);
     TIMER_stop(channel);
     pwms[channel].timer_config.pwmFreq_HZ = 0; // 作为标志位
