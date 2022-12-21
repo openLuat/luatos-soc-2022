f77e5b9e24a4d8ae9c6ad0a17f4ae1c7e9f6d60a
doudou <1491766334@qq.com>
2022-12-21 16:12:21 +0800

    update:千里传音的demo改个名字

--- project/example_link_speech1/xmake.lua
+++ project/example_link_speech1/xmake.lua
@@ -1,4 +1,4 @@
-local TARGET_NAME = "example_ali_qianlichuanyin"
+local TARGET_NAME = "example_link_speech1"
 local LIB_DIR = "$(buildir)/".. TARGET_NAME .. "/"
 local LIB_NAME = "lib" .. TARGET_NAME .. ".a "
 includes(SDK_TOP .. "/thirdparty/audio_decoder")
