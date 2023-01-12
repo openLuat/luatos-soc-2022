d39ff32e1610c82f0dac8a624a13cd748a0f6f10
alienwalker <alienwalker@sina.com>
2023-01-12 14:36:29 +0800

    update:更新I2C驱动，提高TCPIP协议栈的task优先级

--- interface/src/luat_i2c_ec618.c
+++ interface/src/luat_i2c_ec618.c
@@ -19,6 +19,8 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+#if 0
+
 #include "FreeRTOS.h"
 #include "luat_i2c.h"
 #include "common_api.h"
@@ -161,3 +163,156 @@ int luat_i2c_transfer(int id, int addr, uint8_t *reg, size_t reg_len, uint8_t *b
     if (result != 0) return-1;
     return luat_i2c_recv(id, addr, buff, len);
 }
+
+#else
+#include "luat_i2c.h"
+#include "common_api.h"
+#include "soc_i2c.h"
+#include "driver_gpio.h"
+static uint8_t luat_i2c_iomux[I2C_MAX];
+int luat_i2c_exist(int id) {
+    return (id < I2C_MAX);
+}
+
+int luat_i2c_set_iomux(int id, uint8_t value)
+{
+	if (!luat_i2c_exist(id)) return -1;
+	luat_i2c_iomux[id] = value;
+}
+
+int luat_i2c_setup(int id, int speed) {
+    if (speed == 0) {
+        speed = 100 * 1000; // SLOW
+    }
+    else if (speed == 1) {
+        speed = 400 * 1000; // FAST
+    }
+    else if (speed == 2) {
+        speed = 400 * 1000; // SuperFast
+    }
+    if (!luat_i2c_exist(id)) return -1;
+    if (id)
+    {
+    	switch(luat_i2c_iomux[id])
+    	{
+    	case 1:
+        	GPIO_IomuxEC618(23, 2, 1, 0);
+        	GPIO_IomuxEC618(24, 2, 1, 0);
+    		break;
+    	default:
+        	GPIO_IomuxEC618(19, 2, 1, 0);
+        	GPIO_IomuxEC618(20, 2, 1, 0);
+        	break;
+    	}
+
+    }
+    else
+    {
+    	switch(luat_i2c_iomux[id])
+    	{
+    	case 1:
+        	GPIO_IomuxEC618(27, 2, 1, 0);
+        	GPIO_IomuxEC618(28, 2, 1, 0);
+    		break;
+    	case 2:
+        	GPIO_IomuxEC618(16, 2, 1, 0);
+        	GPIO_IomuxEC618(17, 2, 1, 0);
+    		break;
+    	default:
+        	GPIO_IomuxEC618(13, 2, 1, 0);
+        	GPIO_IomuxEC618(14, 2, 1, 0);
+        	break;
+    	}
+
+    }
+	I2C_MasterSetup(id, speed);
+    return 0;
+}
+
+int luat_i2c_close(int id) {
+    if (!luat_i2c_exist(id)) return -1;
+    if (id)
+    {
+    	switch(luat_i2c_iomux[id])
+    	{
+    	case 1:
+        	GPIO_IomuxEC618(23, 0, 1, 0);
+        	GPIO_IomuxEC618(24, 0, 1, 0);
+    		break;
+    	default:
+        	GPIO_IomuxEC618(19, 0, 1, 0);
+        	GPIO_IomuxEC618(20, 0, 1, 0);
+        	break;
+    	}
+
+    }
+    else
+    {
+    	switch(luat_i2c_iomux[id])
+    	{
+    	case 1:
+        	GPIO_IomuxEC618(27, 0, 1, 0);
+        	GPIO_IomuxEC618(28, 0, 1, 0);
+    		break;
+    	case 2:
+        	GPIO_IomuxEC618(16, 0, 1, 0);
+        	GPIO_IomuxEC618(17, 0, 1, 0);
+    		break;
+    	default:
+        	GPIO_IomuxEC618(13, 0, 1, 0);
+        	GPIO_IomuxEC618(14, 0, 1, 0);
+        	break;
+    	}
+
+    }
+    return 0;
+}
+
+int luat_i2c_send(int id, int addr, void* buff, size_t len, uint8_t stop) {
+	if (!luat_i2c_exist(id)) return -1;
+	return I2C_BlockWrite(id, addr, (const uint8_t *)buff, len, 25, NULL, NULL);
+    // I2C_Prepare(id, addr, 1, NULL, NULL);
+    // I2C_MasterXfer(id, I2C_OP_WRITE, 0, buff, len, 20);
+}
+
+int luat_i2c_recv(int id, int addr, void* buff, size_t len) {
+	if (!luat_i2c_exist(id)) return -1;
+	return I2C_BlockRead(id, addr, 0, 0, (uint8_t *)buff, len, 25, NULL, NULL);
+    // I2C_Prepare(id, addr, 1, NULL, NULL);
+    // I2C_MasterXfer(id, I2C_OP_READ, 0, buff, len, 20);
+}
+
+int luat_i2c_transfer(int id, int addr, uint8_t *reg, size_t reg_len, uint8_t *buff, size_t len) {
+	if (!luat_i2c_exist(id)) return -1;
+	if (reg && reg_len) {
+		return I2C_BlockRead(id, addr, reg, reg_len, (uint8_t *)buff, len, 25, NULL, NULL);
+	} else {
+		return I2C_BlockWrite(id, addr, (const uint8_t *)buff, len, 25, NULL, NULL);
+	}
+}
+extern void I2C_SetNoBlock(uint8_t I2CID);
+int luat_i2c_no_block_transfer(int id, int addr, uint8_t is_read, uint8_t *reg, size_t reg_len, uint8_t *buff, size_t len, uint16_t Toms, void *CB, void *pParam) {
+	if (!luat_i2c_exist(id)) return -1;
+	int32_t Result;
+	if (!I2C_WaitResult(id, &Result)) {
+		return -1;
+	}
+	I2C_Prepare(id, addr, 2, CB, pParam);
+	I2C_SetNoBlock(id);
+	if (reg && reg_len)
+	{
+		I2C_MasterXfer(id, I2C_OP_READ_REG, reg, reg_len, buff, len, Toms);
+	}
+	else if (is_read)
+	{
+		I2C_MasterXfer(id, I2C_OP_READ, NULL, 0, buff, len, Toms);
+	}
+	else
+	{
+		I2C_MasterXfer(id, I2C_OP_WRITE, NULL, 0, buff, len, Toms);
+	}
+	return 0;
+}
+
+
+#endif
