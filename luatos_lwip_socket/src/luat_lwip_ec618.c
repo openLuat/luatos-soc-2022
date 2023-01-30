adb8a719759982083826d1c934d9e6d34432c16f
alienwalker <alienwalker@sina.com>
2023-01-30 16:53:31 +0800

    update:csdk的luatos socket初始化时把mbedtls debug设置成0

--- luatos_lwip_socket/src/luat_lwip_ec618.c
+++ luatos_lwip_socket/src/luat_lwip_ec618.c
@@ -769,7 +769,7 @@ void net_lwip_init(void)
 		INIT_LLIST_HEAD(&prvlwip.socket[i].rx_head);
 		prvlwip.socket[i].mutex = platform_create_mutex();
 	}
-
+	mbedtls_debug_set_threshold(0);
 	prvlwip.dns_timer = platform_create_timer(net_lwip_timer_cb, (void *)EV_LWIP_COMMON_TIMER, 0);
 }
 
