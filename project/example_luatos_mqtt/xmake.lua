5efb6f3d4171de26ecfd95f1ca472b44c860b61e
Dozingfiretruck <1473454180@qq.com>
2023-01-30 16:52:40 +0800

    add:迁移luatos的mqtt,推荐使用此mqtt客户端

---
+++ project/example_luatos_mqtt/xmake.lua
@@ -0,0 +1,21 @@
+local TARGET_NAME = "example_luatos_mqtt"
+local LIB_DIR = "$(buildir)/".. TARGET_NAME .. "/"
+local LIB_NAME = "lib" .. TARGET_NAME .. ".a "
+
+includes(SDK_TOP.."/luatos_lwip_socket")
+target(TARGET_NAME)
+    set_kind("static")
+    set_targetdir(LIB_DIR)
+
+    includes(SDK_TOP .. "/thirdparty/libemqtt")
+    add_deps("libemqtt")
+
+    add_includedirs("./inc",{public = true})
+    -- add_includedirs(SDK_TOP .. "/interface/private_include", 
+    --                 {public = true})
+    add_files("./src/*.c",{public = true})
+    
+    --自动链接
+    LIB_USER = LIB_USER .. SDK_TOP .. LIB_DIR .. LIB_NAME .. " "
+    --甚至可以加入自己的库
+target_end()
