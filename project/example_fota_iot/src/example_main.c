613fe59ca4c28420e13ac904dea78a166675e3c7
sunzhipeng <1491766334@qq.com>
2023-02-22 21:27:32 +0800

    update：还原并增加注释

--- project/example_fota_iot/src/example_main.c
+++ project/example_fota_iot/src/example_main.c
@@ -30,18 +30,43 @@
 FOTA应用开发可以参考：https://doc.openluat.com/wiki/37?wiki_page_id=4727
 */
 
+#define PROJECT_VERSION  "1.0.0"                  //使用合宙iot升级的话此字段必须存在，并且强制固定格式为x.x.x, x可以为任意的数字
+#define PROJECT_KEY "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  //修改为自己iot上面的PRODUCT_KEY，这里是一个错误的，使用合宙iot升级的话此字段必须存在
+#define PROJECT_NAME "TEST_FOTA"                  //使用合宙iot升级的话此字段必须存在，可以任意修改，但和升级包的必须一致
 
-#define PROJECT_VERSION  "1.0.0"
-#define PROJECT_KEY "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  //修改为自己iot上面的PRODUCT_KEY，这里是一个错误的
-#define PROJECT_NAME "TEST_FOTA"
+
+/*
+    PROJECT_VERSION:用于区分不同软件版本，同时也用于区分固件差分基础版本
+    假设：
+        现在有两批模块在运行不同的基础版本，一个版本号为1.0.0，另一个版本号为2.0.0
+        现在这两个版本都需要升级到3.0.0，则需要做两个差分包，一个是从1.0.0升级到3.0.0，另一个是从2.0.0升级到3.0.0
+        但是因为合宙IOT是通过firmware来区分不同版本固件，只要请求时firmware相同，版本号比设备运行的要高，就会下发升级文件
+        所以需要对firmware做一点小小的操作，加一个字段来区分基础版本不同的差分包
+        从1.0.0升级到3.0.0生成的firmware字段为1.0.0_TEST_FOTA_CSDK_EC618
+        从2.0.0升级到3.0.0生成的firmware字段为2.0.0_TEST_FOTA_CSDK_EC618
+
+        这样操作后，当两个升级包放上去，1.0.0就算被放进了2.0.0_TEST_FOTA_CSDK_EC618的升级列表里，也会因为自身上报的字段和所在升级列表的固件名称不一致而被服务器拒绝升级
+
+    重要说明！！！！！！！！！！！！！！
+    设备如果升级的话，设备运行的版本必须和差分文件时所选的旧版固件一致
+    举例，用1.0.0和3.0.0差分得软件，必须也只能给运行1.0.0软件的设备用
+*/
 
 char g_test_server_name[200] = {0};
 #define TEST_HOST "http://iot.openluat.com"
 #define HTTP_RECV_BUF_SIZE      (1501)
 #define HTTP_HEAD_BUF_SIZE      (800)
 
+
+
 static HttpClientContext        gHttpClient = {0};
 luat_fota_img_proc_ctx_ptr test_luat_fota_handle = NULL;
+
+
+const char *soc_get_sdk_type(void) //用户可以重新实现这个函数，自定义版本名称
+{
+	return "CSDK";
+}
 
 /**
   \fn      INT32 httpGetData(CHAR *getUrl, CHAR *buf, UINT32 len)
@@ -142,7 +167,7 @@ static void task_test_fota(void *param)
     gHttpClient.ignore = 1;
     char imei[16] = {0};
     luat_mobile_get_imei(0, imei, 15);
-    snprintf(g_test_server_name, 200, "%s/api/site/firmware_upgrade?project_key=%s&imei=%s&device_key=&firmware_name=%s_%s_%s&version=%s", TEST_HOST, PROJECT_KEY, imei, PROJECT_NAME, soc_get_sdk_type(), "EC618", PROJECT_VERSION);
+    snprintf(g_test_server_name, 200, "%s/api/site/firmware_upgrade?project_key=%s&imei=%s&device_key=&firmware_name=%s_%s_%s_%s&version=%s", TEST_HOST, PROJECT_KEY, imei, PROJECT_VERSION, PROJECT_NAME, soc_get_sdk_type(), "EC618", PROJECT_VERSION);
     LUAT_DEBUG_PRINT("test print url %s", g_test_server_name);
     luat_rtos_semaphore_take(net_semaphore_handle, LUAT_WAIT_FOREVER);
 
