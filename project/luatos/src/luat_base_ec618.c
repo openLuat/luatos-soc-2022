771d1adde99d06bc46c759aa3c97d89ce2494a90
alienwalker <alienwalker@sina.com>
2023-03-11 13:27:23 +0800

    update:luatos 64bit库加宏来控制

--- project/luatos/src/luat_base_ec618.c
+++ project/luatos/src/luat_base_ec618.c
@@ -239,7 +239,9 @@ static const luaL_Reg loadedlibs[] = {
 #ifdef LUAT_USE_MAX30102
   {"max30102", luaopen_max30102},
 #endif
+#ifdef LUAT_USE_BIT64
   {"bit64", luaopen_bit64},
+#endif
   {NULL, NULL}
 };
 
