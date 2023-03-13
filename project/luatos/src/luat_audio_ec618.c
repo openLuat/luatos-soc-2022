33d13e29b6b6af6e45f3a149e1149f45510adb13
alienwalker <alienwalker@sina.com>
2023-03-13 11:51:36 +0800

    add:luatos也能用软件DAC了

--- project/luatos/src/luat_audio_ec618.c
+++ project/luatos/src/luat_audio_ec618.c
@@ -337,6 +337,16 @@ uint16_t luat_audio_vol(uint8_t multimedia_id, uint16_t vol)
 	return g_s_audio_hardware.vol;
 }
 
+/**
+ * @brief 设置音频硬件输出类型
+ *
+ * @param bus_type 见MULTIMEDIA_AUDIO_BUS，目前只有0=DAC 1=I2S 2=SOFT_DAC
+ */
+void luat_audio_set_bus_type(uint8_t bus_type)
+{
+	audio_play_set_bus_type(bus_type);
+}
+
 int luat_i2s_setup(luat_i2s_conf_t *conf)
 {
 	if (conf->id >= I2S_MAX) return -1;
