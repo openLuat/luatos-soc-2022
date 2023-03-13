9b6f6f64fb76c4637545e39cfb0a4cf19a7819d2
Dozingfiretruck <1473454180@qq.com>
2023-03-13 20:34:59 +0800

    add:生成对应版本名

--- build_luatos_full.bat
+++ build_luatos_full.bat
@@ -1,2 +1,4 @@
 build.bat luatos
+del .\out\luatos\LuatOS-SoC_V1104_EC618_FULL.soc
+copy .\out\luatos\LuatOS-SoC_V1104_EC618.soc .\out\luatos\LuatOS-SoC_V1104_EC618_FULL.soc
 pause
--- build_luatos_lite.bat
+++ build_luatos_lite.bat
@@ -1,4 +1,6 @@
 set LUAT_EC618_LITE_MODE=1
 build.bat luatos
 set LUAT_EC618_LITE_MODE=0
+del .\out\luatos\LuatOS-SoC_V1104_EC618_LITE.soc
+copy .\out\luatos\LuatOS-SoC_V1104_EC618.soc .\out\luatos\LuatOS-SoC_V1104_EC618_LITE.soc
 pause
