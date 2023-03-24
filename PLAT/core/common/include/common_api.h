75625d47c8d2b6bd14bb46ed2c6a8b47571df587
alienwalker <alienwalker@sina.com>
2023-03-24 17:25:36 +0800

    update:更新注释

--- PLAT/core/common/include/common_api.h
+++ PLAT/core/common/include/common_api.h
@@ -67,20 +67,20 @@ int soc_usb_serial_output(uint8_t channel, uint8_t *output, uint32_t len);
 void soc_set_pad_wakeup_callback(pad_wakeup_fun_t cb_fun);
 #define set_pad_wakeup_callback(x)	soc_set_pad_wakeup_callback(x)
 /**
- * @brief 创建一个带event收发机制的task，event就是一个16byte的queue，在创建task的时候，同时创建好了一个queue，event结构见OS_EVENT
+ * @brief 创建一个带event收发机制的task，event就是一个16byte的item
  * 
  * @param task_fun task的入口函数
  * @param param task的入口参数
  * @param stack_bytes task的堆栈长度，单位byte，会强制4字节对齐
  * @param priority task的任务优先级，注意是百分比，0~100，100为底层OS允许的最高级，0为底层OS允许的最低级
  * @param task_name task的name
- * @param event_max_cnt，如果OS不带mailbox，就需要本参数来创建queue
+ * @param event_max_cnt，如果OS允许在中断里malloc，这个参数无效，如果OS不允许在中断里malloc，则这里填写预分配的event空间用于中断里使用，如果写0会使用公共event
  * @return void* task的句柄，后续收发event都需要这个参数，NULL为创建失败
  */
 void *create_event_task(TaskFun_t task_fun, void *param, uint32_t stack_bytes, uint8_t priority, uint16_t event_max_cnt, const char *task_name);
 
 /**
- * @brief 删除掉一个带event收发机制的task，比正常删除task多了一步删除event queue。
+ * @brief 删除掉一个带event收发机制的task，比正常删除task多了一步删除event mailbox。
  * 
  * @param task_handle task的句柄
  */
@@ -95,13 +95,13 @@ void delete_event_task(void *task_handle);
  * @param param1 需要构建的param1
  * @param param2 需要构建的param2
  * @param param3 需要构建的param3
- * @param timeout_ms 发送的超时时间，0不等待，0xffffffff永远等待，建议就直接写0
- * @return int 成功返回0，其他都是失败
+ * @param timeout_ms 这里已经废弃了
+ * @return int 成功返回0，其他都是失败，现在只有在中断里预分配的event用完了才会返回失败
  */
 int send_event_to_task(void *task_handle, OS_EVENT *event, uint32_t event_id, uint32_t param1, uint32_t param2, uint32_t param3, uint32_t timeout_ms);
 
 /**
- * @brief 获取一个event，并根据需要返回
+ * @brief 获取一个event，并根据需要返回，如果缓存了超过1024个event会断言
  * 如果target_event_id != 0 && != 0xffffffff，那么收到对应event id时返回，如果不是，则由callback交给用户临时处理，如果callback为空，则抛弃掉
  * 如果target_event_id == 0，收到消息就返回
  * 如果target_event_id == 0xffffffff，收到消息则由callback交给用户临时处理，如果callback为空，则抛弃掉
@@ -114,6 +114,15 @@ int send_event_to_task(void *task_handle, OS_EVENT *event, uint32_t event_id, ui
  * @return int 收到需要的event返回0
  */
 int get_event_from_task(void *task_handle, uint32_t target_event_id, OS_EVENT *event,  CBFuncEx_t callback, uint32_t timeout_ms);
+
+/**
+ * @brief 获取task里未处理event的数量
+ *
+ * @param task_handle task的句柄
+ * @return uint32_t task里未处理event的数量
+ */
+uint32_t get_event_cnt(void *task_handle);
+
 
 /**
  * @brief 获取开机到现在的ms时间
