ebd95368227d34e774e3a87b6ebcc9a009265cd9
Dozingfiretruck <1473454180@qq.com>
2023-05-06 15:05:47 +0800

    add:sfud demo 添加lfs文件系统

--- project/example_sfud/src/example_main.c
+++ project/example_sfud/src/example_main.c
@@ -26,8 +26,44 @@
 #include "luat_spi.h"
 #include "sfud.h"
 
+#include "common_api.h"
+#include "bsp_custom.h"
+
+#include "lfs.h"
+
 luat_rtos_task_handle sfud_task_handle;
 
+
+#define LFS_BLOCK_SIZE (4096)
+#define LFS_BLOCK_DEVICE_READ_SIZE (256)
+#define LFS_BLOCK_DEVICE_PROG_SIZE (256)
+#define LFS_BLOCK_DEVICE_CACHE_SIZE (256)
+#define LFS_BLOCK_DEVICE_ERASE_SIZE (4096) // one sector 4KB
+#define LFS_BLOCK_DEVICE_TOTOAL_SIZE (FLASH_FS_REGION_SIZE)
+#define LFS_BLOCK_DEVICE_LOOK_AHEAD (16)
+
+
+static int sfud_block_device_read(const struct lfs_config *cfg, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size) {
+    sfud_flash* flash = (sfud_flash*)cfg->context;
+    int ret = sfud_read(flash, block * LFS_BLOCK_SIZE + off, size, buffer);
+    return LFS_ERR_OK;
+}
+
+static int sfud_block_device_prog(const struct lfs_config *cfg, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size) {
+    sfud_flash* flash = (sfud_flash*)cfg->context;
+    int ret = sfud_write(flash, block * LFS_BLOCK_SIZE + off, size, buffer);
+    return LFS_ERR_OK;
+}
+
+static int sfud_block_device_erase(const struct lfs_config *cfg, lfs_block_t block) {
+    sfud_flash* flash = (sfud_flash*)cfg->context;
+    int ret = sfud_erase(flash, block * LFS_BLOCK_SIZE, LFS_BLOCK_SIZE);
+    return LFS_ERR_OK;
+}
+
+static int sfud_block_device_sync(const struct lfs_config *cfg) {
+    return LFS_ERR_OK;
+}
 
 luat_spi_t sfud_spi_flash = {
         .id = 0,
@@ -37,37 +73,67 @@ luat_spi_t sfud_spi_flash = {
         .bit_dict = 0,
         .master = 1,
         .mode = 0,
-        .bandrate=25*1000*1000,
+        .bandrate=25600000,
         .cs = 8
 };
 
-uint8_t send_data[64] = {0};
-uint8_t read_data[64] = {0};
-
-#include "common_api.h"
-#include "bsp_custom.h"
+// variables used by the filesystem
+lfs_t lfs;
+lfs_file_t file;
 
 static void task_test_sfud(void *param)
 {
+
     int re = -1;
-    uint8_t data[8] = {0};
     luat_spi_setup(&sfud_spi_flash);
 
     if (re = sfud_init()!=0){
         LUAT_DEBUG_PRINT("sfud_init error is %d\n", re);
     }
     const sfud_flash *flash = sfud_get_device_table();
-    if (re = sfud_erase(flash,1024, 6)!=0){
-        LUAT_DEBUG_PRINT("sfud_erase error is %d\n", re);
+
+    // configuration of the filesystem is provided by this struct
+    const struct lfs_config sfud_lfs_cfg = {
+        .context = flash,
+        // block device operations
+        .read  = sfud_block_device_read,
+        .prog  = sfud_block_device_prog,
+        .erase = sfud_block_device_erase,
+        .sync  = sfud_block_device_sync,
+
+        // block device configuration
+        .read_size = LFS_BLOCK_DEVICE_READ_SIZE,
+        .prog_size = LFS_BLOCK_DEVICE_PROG_SIZE,
+        .block_size = flash->chip.erase_gran,
+        .block_count = flash->chip.capacity / flash->chip.erase_gran,
+        .cache_size = LFS_BLOCK_DEVICE_CACHE_SIZE,
+        .lookahead_size = LFS_BLOCK_DEVICE_LOOK_AHEAD,
+        .block_cycles = 200,
+    };
+
+    // mount the filesystem
+    int err = lfs_mount(&lfs, &sfud_lfs_cfg);
+    // reformat if we can't mount the filesystem
+    // this should only happen on the first boot
+    if (err) {
+        LUAT_DEBUG_PRINT("lfs_mount err: %d\n", err);
+        lfs_format(&lfs, &sfud_lfs_cfg);
+        lfs_mount(&lfs, &sfud_lfs_cfg);
     }
-    if (re = sfud_write(flash, 1024, 6, "luatos")!=0){
-        LUAT_DEBUG_PRINT("sfud_write error is %d\n", re);
-    }
-    if (re = sfud_read(flash, 1024, 6, &data)!=0){
-        LUAT_DEBUG_PRINT("sfud_read error is %d\n", re);
-    }else{
-        LUAT_DEBUG_PRINT("sfud_read %s\n", data);
-    }
+    // read current count
+    uint32_t boot_count = 0;
+    lfs_file_open(&lfs, &file, "boot_count", LFS_O_RDWR | LFS_O_CREAT);
+    lfs_file_read(&lfs, &file, &boot_count, sizeof(boot_count));
+    // update boot count
+    boot_count += 1;
+    lfs_file_rewind(&lfs, &file);
+    lfs_file_write(&lfs, &file, &boot_count, sizeof(boot_count));
+    // remember the storage is not updated until the file is closed successfully
+    lfs_file_close(&lfs, &file);
+    // release any resources we were using
+    lfs_unmount(&lfs);
+    // print the boot count
+    LUAT_DEBUG_PRINT("boot_count: %d\n", boot_count);
     
     while (1)
     {
@@ -89,7 +155,7 @@ static void task_demo_sfud(void)
 	// luat_gpio_open(&gpio_cfg);
 	// luat_gpio_set(FLASH_EN, LUAT_GPIO_HIGH);
 
-    luat_rtos_task_create(&sfud_task_handle, 1024, 20, "sfud", task_test_sfud, NULL, NULL);
+    luat_rtos_task_create(&sfud_task_handle, 2048, 20, "sfud", task_test_sfud, NULL, NULL);
 }
 
 INIT_TASK_EXPORT(task_demo_sfud,"1");
