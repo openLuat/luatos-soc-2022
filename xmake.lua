fcb2ad7f775ad76555c1e26d3e59b02ebcbbb4ba
Wendal Chen <wendal1985@gmail.com>
2023-05-15 20:03:35 +0800

    fix: luatos固件会编译失败

--- xmake.lua
+++ xmake.lua
@@ -375,12 +375,12 @@ target(USER_PROJECT_NAME..".elf")
     if USER_PROJECT_NAME ~= 'luatos' then
         add_files(SDK_TOP.."/thirdparty/fal/src/*.c",{public = true})
         add_files(SDK_TOP.."/thirdparty/flashdb/src/*.c",{public = true})
+        add_files(SDK_TOP .. "/thirdparty/vfs/*.c",{public = true})
     else
         remove_files(SDK_TOP .. "/interface/src/luat_kv_ec618.c"
 	)
     end
     add_files(SDK_TOP .. "/thirdparty/littlefs/**.c",{public = true})
-    add_files(SDK_TOP .. "/thirdparty/vfs/*.c",{public = true})
 
 	add_ldflags(LD_BASE_FLAGS .. " -Wl,--whole-archive -Wl,--start-group " .. LIB_BASE .. LIB_USER .. " -Wl,--end-group -Wl,--no-whole-archive -Wl,--no-undefined -Wl,--no-print-map-discarded  -ldriver", {force=true})
 	
