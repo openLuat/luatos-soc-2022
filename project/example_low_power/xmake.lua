ec73272973af89aa5a8052e76b479741d7754ec2
sunzhipeng <1491766334@qq.com>
2023-06-15 16:12:38 +0800

    add:psm plus demo

--- project/example_low_power/xmake.lua
+++ project/example_low_power/xmake.lua
@@ -12,6 +12,7 @@ target(TARGET_NAME)
     add_files("./src/example_main.c",{public = true})
     -- add_files("./src/example_ipv6_wakeup.c",{public = true})
     -- add_files("./src/remote_net_wakeup.c",{public = true})
+    -- add_files("./src/psm_plus.c",{public = true})
 
     --路径可以随便写,可以加任意路径的代码,下面代码等效上方代码
     -- add_includedirs(SDK_TOP .. "project/" .. TARGET_NAME .. "/inc",{public = true})
