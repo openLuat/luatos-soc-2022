da8256837cc899a00f6ae39c453e7b46a472c505
alienwalker <alienwalker@sina.com>
2023-07-18 11:07:18 +0800

    add:fatfs demo，spi1读写tf卡

---
+++ project/example_fatfs/inc/luat_fatfs_config.h
@@ -0,0 +1,13 @@
+/*
+ * luat_fatfs_config.h
+ *
+ *  Created on: 2023年7月18日
+ *      Author: Administrator
+ */
+
+#ifndef LVGL_DEMO_INC_LUAT_FATFS_CONFIG_H_
+#define LVGL_DEMO_INC_LUAT_FATFS_CONFIG_H_
+//#define LUAT_USE_FATFS_CHINESE
+
+
+#endif /* LVGL_DEMO_INC_LUAT_FATFS_CONFIG_H_ */
