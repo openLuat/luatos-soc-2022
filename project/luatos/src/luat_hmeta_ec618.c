7f6b4b881115511aaf73bf32c24abeb92cb9ee36
Wendal Chen <wendal1985@gmail.com>
2023-07-25 15:01:05 +0800

    add: 添加luat_hmeta_hwversion函数,获取硬件版本号

--- project/luatos/src/luat_hmeta_ec618.c
+++ project/luatos/src/luat_hmeta_ec618.c
@@ -15,3 +15,24 @@ int luat_hmeta_model_name(char* buff) {
     }
     return soc_get_model_name(buff, 0);
 }
+
+
+int luat_hmeta_hwversion(char* buff2) {
+    char buff[128] = {0};
+    int ret = soc_get_model_name(buff, 1);
+    if (ret != 0) {
+        uint64_t ticks = luat_mcu_tick64_ms();
+        if (ticks < 250) {
+            luat_rtos_task_sleep(250 - ticks);
+        }
+        soc_get_model_name(buff, 1);
+    }
+    for (size_t i = 0; i < strlen(buff); i++)
+    {
+        if (buff[i] == '_') {
+            memcpy(buff2, buff + i + 1, strlen(buff) - i);
+            return 0;
+        }
+    }
+    return -1;
+}
