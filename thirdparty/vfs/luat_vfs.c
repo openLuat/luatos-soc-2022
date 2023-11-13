70a4284ddd9f9ce62472e1350e2b906c5fbe5486
Wendal Chen <wendal1985@gmail.com>
2023-11-13 16:44:08 +0800

    fix: vfs下luat_fs_truncate重复定义

--- thirdparty/vfs/luat_vfs.c
+++ thirdparty/vfs/luat_vfs.c
@@ -170,18 +170,6 @@ int luat_fs_init(void) {
 extern luat_vfs_mount_t * getmount(const char* filename);
 extern int luat_vfs_lfs2_truncate(void* userdata, const char *filename, size_t len);
 
-int luat_fs_truncate(const char* filename, size_t len) {
-    luat_vfs_mount_t *mount = getmount(filename);
-    if (mount == NULL ) return -1;
-    if (strcmp(mount->fs->name, "lfs2") == 0) {
-        return luat_vfs_lfs2_truncate(mount->userdata, filename + strlen(mount->prefix), len);
-    }else if(strcmp(mount->fs->name, "ec618") == 0){
-        return luat_vfs_ec618_truncate(NULL, filename, len);
-    }
-    return -1;
-}
-
-
 int luat_fs_mkfs(luat_fs_conf_t *conf) {
     for (size_t j = 0; j < LUAT_VFS_FILESYSTEM_MOUNT_MAX; j++) {
         if (vfs.mounted[j].ok == 0)
