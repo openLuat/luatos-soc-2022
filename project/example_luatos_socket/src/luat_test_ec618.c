1f896a7d48c785e854767fbf0b2b14190698c6e9
alienwalker <alienwalker@sina.com>
2024-07-29 15:30:51 +0800

    update:基站同步时间从第二次开始进行有效性判断

--- project/example_luatos_socket/src/luat_test_ec618.c
+++ project/example_luatos_socket/src/luat_test_ec618.c
@@ -35,18 +35,133 @@ static int32_t luat_server_test_socket_callback(void *pdata, void *param)
 
 static void luatos_mobile_event_callback(LUAT_MOBILE_EVENT_E event, uint8_t index, uint8_t status)
 {
-	if (LUAT_MOBILE_EVENT_NETIF == event)
+	luat_mobile_cell_info_t cell_info;
+	luat_mobile_signal_strength_info_t signal_info;
+	uint8_t csq, i;
+	char imsi[20];
+	char iccid[24] = {0};
+	char apn[32] = {0};
+	ip_addr_t ipv4;
+	ip_addr_t ipv6;
+	switch(event)
 	{
-		if (LUAT_MOBILE_NETIF_LINK_ON == status)
+	case LUAT_MOBILE_EVENT_CFUN:
+		LUAT_DEBUG_PRINT("CFUN消息，status %d", status);
+		break;
+	case LUAT_MOBILE_EVENT_SIM:
+		if (status != LUAT_MOBILE_SIM_NUMBER)
 		{
-			uint8_t is_ipv6;
-			luat_socket_check_ready(index, &is_ipv6);
-			if (is_ipv6)
-			{
-				g_s_server_ip = *net_lwip_get_ip6();
-				LUAT_DEBUG_PRINT("%s", ipaddr_ntoa(&g_s_server_ip));
-			}
+			LUAT_DEBUG_PRINT("SIM卡消息，卡槽%d", index);
 		}
+		switch(status)
+		{
+		case LUAT_MOBILE_SIM_READY:
+			LUAT_DEBUG_PRINT("SIM卡正常工作");
+			luat_mobile_get_iccid(index, iccid, sizeof(iccid));
+			LUAT_DEBUG_PRINT("ICCID %s", iccid);
+			luat_mobile_get_imsi(index, imsi, sizeof(imsi));
+			LUAT_DEBUG_PRINT("IMSI %s", imsi);
+			break;
+		case LUAT_MOBILE_NO_SIM:
+			LUAT_DEBUG_PRINT("SIM卡不存在");
+			break;
+		case LUAT_MOBILE_SIM_NEED_PIN:
+			LUAT_DEBUG_PRINT("SIM卡需要输入PIN码");
+			break;
+		}
+		break;
+	case LUAT_MOBILE_EVENT_REGISTER_STATUS:
+		LUAT_DEBUG_PRINT("移动网络服务状态变更，当前为%d", status);
+		break;
+	case LUAT_MOBILE_EVENT_CELL_INFO:
+		switch(status)
+		{
+		case LUAT_MOBILE_CELL_INFO_UPDATE:
+			LUAT_DEBUG_PRINT("周期性搜索小区信息完成一次");
+			luat_mobile_get_last_notify_cell_info(&cell_info);
+			if (cell_info.lte_service_info.cid)
+			{
+				LUAT_DEBUG_PRINT("服务小区信息 mcc %x mnc %x cellid %u band %d tac %u pci %u earfcn %u is_tdd %d rsrp %d rsrq %d snr %d rssi %d",
+						cell_info.lte_service_info.mcc, cell_info.lte_service_info.mnc, cell_info.lte_service_info.cid,
+						cell_info.lte_service_info.band, cell_info.lte_service_info.tac, cell_info.lte_service_info.pci, cell_info.lte_service_info.earfcn,
+						cell_info.lte_service_info.is_tdd, cell_info.lte_service_info.rsrp, cell_info.lte_service_info.rsrq,
+						cell_info.lte_service_info.snr, cell_info.lte_service_info.rssi);
+			}
+			for (i = 0; i < cell_info.lte_neighbor_info_num; i++)
+			{
+				if (cell_info.lte_info[i].cid)
+				{
+					LUAT_DEBUG_PRINT("邻小区 %d mcc %x mnc %x cellid %u tac %u pci %u", i + 1, cell_info.lte_info[i].mcc,
+							cell_info.lte_info[i].mnc, cell_info.lte_info[i].cid, cell_info.lte_info[i].tac, cell_info.lte_info[i].pci);
+					LUAT_DEBUG_PRINT("邻小区 %d earfcn %u rsrp %d rsrq %d snr %d", i + 1, cell_info.lte_info[i].earfcn, cell_info.lte_info[i].rsrp,
+							cell_info.lte_info[i].rsrq, cell_info.lte_info[i].snr);
+				}
+			}
+			break;
+		case LUAT_MOBILE_SIGNAL_UPDATE:
+			LUAT_DEBUG_PRINT("服务小区信号状态变更");
+			luat_mobile_get_last_notify_signal_strength_info(&signal_info);
+			luat_mobile_get_last_notify_signal_strength(&csq);
+			if (signal_info.luat_mobile_lte_signal_strength_vaild)
+			{
+				LUAT_DEBUG_PRINT("rsrp %d, rsrq %d, snr %d, rssi %d, csq %d %d", signal_info.lte_signal_strength.rsrp,
+						signal_info.lte_signal_strength.rsrq, signal_info.lte_signal_strength.snr,
+						signal_info.lte_signal_strength.rssi, csq, luat_mobile_rssi_to_csq(signal_info.lte_signal_strength.rssi));
+			}
+
+			break;
+		}
+		break;
+	case LUAT_MOBILE_EVENT_PDP:
+		LUAT_DEBUG_PRINT("CID %d PDP激活状态变更为 %d", index, status);
+		break;
+	case LUAT_MOBILE_EVENT_NETIF:
+		LUAT_DEBUG_PRINT("internet工作状态变更为 %d,cause %d", status,index);
+		switch (status)
+		{
+		case LUAT_MOBILE_NETIF_LINK_ON:
+			LUAT_DEBUG_PRINT("可以上网");
+			luat_socket_check_ready(index, &ipv6);
+			if (luat_mobile_get_apn(0, 0, apn, sizeof(apn)))
+			{
+				LUAT_DEBUG_PRINT("默认apn %s", apn);
+			}
+			luat_mobile_get_local_ip(0, 1, &ipv4, &ipv6);
+			if (ipv4.type != 0xff)
+			{
+				LUAT_DEBUG_PRINT("IPV4 %s", ip4addr_ntoa(&ipv4.u_addr.ip4));
+			}
+			if (ipv6.type != 0xff)
+			{
+				LUAT_DEBUG_PRINT("IPV6 %s", ip6addr_ntoa(&ipv4.u_addr.ip6));
+			}
+			break;
+		default:
+			LUAT_DEBUG_PRINT("不能上网");
+			break;
+		}
+		break;
+	case LUAT_MOBILE_EVENT_TIME_SYNC:
+		if (!status)
+		{
+			LUAT_DEBUG_PRINT("通过移动网络同步了UTC时间");
+		}
+		else
+		{
+			LUAT_DEBUG_PRINT("移动网络同步UTC时间出错");
+		}
+		break;
+	case LUAT_MOBILE_EVENT_CSCON:
+		LUAT_DEBUG_PRINT("RRC状态 %d", status);
+		break;
+	case LUAT_MOBILE_EVENT_NAS_ERROR:
+		LUAT_DEBUG_PRINT("NAS异常类型 %d，拒绝原因 %d", index, status);
+		break;
+	case LUAT_MOBILE_EVENT_FATAL_ERROR:
+		LUAT_DEBUG_PRINT("网络需要严重故障，建议在5秒后重启协议栈");
+		break;
+	default:
+		break;
 	}
 }
 
@@ -65,9 +180,10 @@ static void luat_test_task(void *param)
 	network_init_ctrl(g_s_network_ctrl, g_s_task_handle, luat_test_socket_callback, NULL);
 	network_set_base_mode(g_s_network_ctrl, 1, 15000, 1, 300, 5, 9);
 	g_s_network_ctrl->is_debug = 1;	//下行测速时关闭debug，如果只是普通测试，打开debug
+	luat_mobile_set_rrc_auto_release_time(3);
 	// 请访问 https://netlab.luatos.com 获取新的端口号
 	const char remote_ip[] = "112.125.89.8";
-	int port = 44535;
+	int port = 44966;
 	const char hello[] = "hello, luatos!";
 	uint8_t *tx_data = malloc(1024);
 	uint8_t *rx_data = malloc(1024 * 8);
